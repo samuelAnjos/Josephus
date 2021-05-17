5ecaba40a57abb536a06f9e4b10297e25618d36e
Samuel Anjos <samuelsillva990@gmail.com>
2021-05-16 21:48:18 -0300

    Structs iniciais

--- Josephus/main.c
+++ Josephus/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct no{
+   char nome[20];
+   struct no *proxNo;
+}tipoNo;
+
+typedef struct listaGerenciadora{
+    tipoNo *fim;
+    int qtdade;
+}tipoLista;
+
+void inicializarLista(tipoLista *lista){
+    lista->fim = NULL;
+    lista->qtdade = 0;
+}
+
 int main(){
 
     //Josephus
